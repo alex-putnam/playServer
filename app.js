93ed2846bed927fb6cfafa8a4dc79b9bc9196d76
Alex Putnam <a.kk.putnam@gmail.com>
2020-10-22 13:55:22 -0700

    refactored code/added tests

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+const games = require('./playstore.js');
+
+app.get('/apps', (req, res) => {
+  const { search = '', sort, genres } = req.query;
+
+  if (sort) {
+    if (!['Rating', 'App'].includes(sort)) {
+      return res.status(400).send('Sort must be of genre or app title.');
+    }
+  }
+
+  if (genres) {
+    if (
+      !['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(
+        genres
+      )
+    ) {
+      return res
+        .status(400)
+        .send('Genre must be Action, Puzzle, Strategy, Casual, Arcade, Card');
+    }
+  }
+
+  let results = games.filter((game) =>
+    game.App.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+
+  if (genres) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+
+  if (genres) {
+    results = results.filter((game) => game.Genres.includes(genres));
+  }
+
+  res.json(results);
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,48 +1,5 @@
-const express = require('express');
-const morgan = require('morgan');
-
-const app = express();
-
-app.use(morgan('dev'));
-
-const apps = require('./playstore.js');
-
-app.get('/app', (req, res) => {
-  const { sort, genres = '' } = req.query;
-
-  if (genres) {
-    if (
-      !['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(
-        genres
-      )
-    ) {
-      return res
-        .status(400)
-        .send(
-          'Genres must be Action, Puzzle, Strategy, Casual, Arcade, or Card'
-        );
-    }
-  }
-
-  if (sort) {
-    if (!['rating', 'app'].includes(sort)) {
-      return res.status(400).send('Sort must be rating or app');
-    }
-  }
-
-  let results = apps.filter((app) =>
-    app.Genres.toLowerCase().includes(genres.toLowerCase())
-  );
-
-  if (sort) {
-    results.sort((a, b) => {
-      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-    });
-  }
-
-  res.json(results);
-});
+const app = require('./app');
 
 app.listen(8000, () => {
-  console.log('Server started on PORT 8000');
+  console.log('Server started, listening on http://localhost:8000');
 });
